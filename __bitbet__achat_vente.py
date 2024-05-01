e9e93ff9c4ac23f4110c961b2d433b4d575df789
VadimKashtanov <59579094+VadimKashtanov@users.noreply.github.com>
2024-05-01 13:12:06 +0200

    Update __bitbet__achat_vente.py

--- __bitbet__achat_vente.py
+++ __bitbet__achat_vente.py
@@ -1,10 +1,7 @@
 from os import system
 from pybitget import Client
 
-client = Client(
-	'bg_b13daa6f909560dd8dfac7549e9ce83f',
-	'd1c9e1c5c520a74b89133945f79246929e27908187cdb2fd51bab280b0dd98e9',
-	'Henrya10ans')
+client = Client()
 
 argent = lambda : float(client.mix_get_account('BTCUSDT_UMCBL', 'USDT')['data']['usdtEquity'])
 
